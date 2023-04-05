73c43abca8f414aea8cfd300bddc979f125dd4ca
Alexis Contour <alex@glumol.com>
2023-04-05 12:27:15 +0200

    Initial Commit

---
+++ RestoFlash/RestoFlash.h
@@ -0,0 +1,18 @@
+//
+//  RestoFlash.h
+//  RestoFlash
+//
+//  Created by Alexis Contour on 05/04/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RestoFlash.
+FOUNDATION_EXPORT double RestoFlashVersionNumber;
+
+//! Project version string for RestoFlash.
+FOUNDATION_EXPORT const unsigned char RestoFlashVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RestoFlash/PublicHeader.h>
+
+
